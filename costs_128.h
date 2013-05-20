aaf1d54044745dd471353898b84f690e1cfd9fb3
Maxwell Sayles <maxwellsayles@gmail.com>
2013-05-20 12:34:21 -0700

    a wee bit of documentation so that i might remember when i use this much later

--- costs_128.h
+++ costs_128.h
@@ -2,5 +2,7 @@
 
 #include "liboptarith/group.h"
 
+/// Index 0 is for 16-bit discriminants, and this only
+/// goes as high as 118-bit discriminants.
 extern const group_cost_t s128_qform_all_costs[];
 
--- costs_64.h
+++ costs_64.h
@@ -2,5 +2,7 @@
 
 #include "liboptarith/group.h"
 
+/// Index 0 is for 16-bit discriminants, and this
+/// only goes as high as 59-bit discriminants.
 extern const group_cost_t s64_qform_all_costs[];
 
