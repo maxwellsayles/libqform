74058751bd7e6a8397e2c188cafb546d9ddcd432
Maxwell Sayles <maxwellsayles@gmail.com>
2013-05-20 12:32:59 -0700

    updated timings for 128bit cubing and source files with timings for each individual discriminant size

---
+++ costs_128.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "liboptarith/group.h"
+
+extern const group_cost_t s128_qform_all_costs[];
+
---
+++ costs_64.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "liboptarith/group.h"
+
+extern const group_cost_t s64_qform_all_costs[];
+
